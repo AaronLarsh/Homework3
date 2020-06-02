9b1d4cdd053786ff4dd651f583336132aedfb95a
Dr Dougie <aaron.m.larsh@gmail.com>
2020-06-01 18:18:05 -0700

    cleaned up comments on js file.

--- script.js
+++ script.js
@@ -32,38 +32,13 @@ function generatePassword() {
     } else {
       //empty array that will add getRandom fucntions if true
       var randomFunctArrayF = new Array();
-     /* console.log(randomFunctArrayF)
-      console.log(charUpper)
-      //if fucntion, checks if var value is true, if true then it will push function value onto array
-      if(charUpper) {
-         randomFunctArrayF.push(getRandomUpper());
-      }
-      if(charLower) {
-        randomFunctArrayF.push(getRandomLower());
-      }
-      if(charNumb) {
-        randomFunctArrayF.push(getRandomNumber());
-      }
-      if(charSpecChar) {
-        randomFunctArrayF.push(getRandomSpecialChar());
-      }
-      console.log(randomFunctArrayF.values[0])
-      console.log(randomFunctArrayF[0])
-      console.log(randomFunctArrayF[0])
-// end of if 
-
-      //function to randomize the array
-      function randomizeCharOrder() {
-        return randomFunctArrayF[Math.floor(Math.random() * randomFunctArrayF.length)];
-      };
-      console.log(randomizeCharOrder())*/
-      
 //var I is equal to charlength
       var i = charLength;
       //var for empty string
       var passwordText = "";
       //loop based on chralenth value, will looop function randomizecharorder and add to empty string
       for (i = 0; i < charLength; i++) {
+        //if fucntion, checks if var value is true, if true then it will push function value onto array
         if(charUpper) {
           randomFunctArrayF.push(getRandomUpper());
        }
@@ -76,6 +51,8 @@ function generatePassword() {
        if(charSpecChar) {
          randomFunctArrayF.push(getRandomSpecialChar());
        }
+       // end of if 
+      //function to randomize the array
         function randomizeCharOrder() {
           return randomFunctArrayF[Math.floor(Math.random() * randomFunctArrayF.length)];
         };
@@ -92,52 +69,21 @@ function generatePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-//1 listen event for click
-//2 prompt
-  //2.1 promted asking character length
-    //2.1.1 must validate be between 8-128
-  //2.2 prompt asking chacters to include
-    //2.2.1 prompt asking about lowercase
-    //2.2.2 prompt asking about uppercase
-    //2.2.3 prompt asking about numbers
-    //2.2.4 prompt asking about special characters
-//3 must validate if all prompts were answered
-  //3.1 validate charlength answered
-    //3.1.1 validate charlength is between 8-128
-  //3.2 validate atleast one char choice picked
-      //3.2.1 check if lc is true
-      //3.2.2 check if uc is true
-      //3.2.3 check if # is true
-      //3.2.4 check if sc is true
-
-//4 generate random char functions
-  //4.1 if lowercase is true generate function
-    //4.1.1 generates random lower case character
-
+//generate random char functions
+    //generates random lower case character
     function getRandomLower() {
       return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
     };
-    console.log(getRandomLower());
-
-  //4.2 if upper case is true generate function
-    //4.2.1 generates random upper case character
+  //generates random upper case character
     function getRandomUpper() {
       return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
     };
-    console.log(getRandomUpper());
-
-  //4.3 if # is true generate function
-    //4.3.1 generates random number character
+  //generates random number character
     function getRandomNumber() {
       return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
     };
-    console.log(getRandomNumber());
-
-  //4.4 if sc is true generate function
-    //4.4.1 generates random special character
+  //generates random special character
     function getRandomSpecialChar() {
       var specialCharList = " !\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~"
       return specialCharList[Math.floor(Math.random() * specialCharList.length)]
     };
-    console.log(getRandomSpecialChar());
